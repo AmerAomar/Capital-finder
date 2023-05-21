369e864cf16c5ed166814232508345e0ac173469
AmerAomar <ameralamriii87@gmail.com>
2023-05-21 18:35:56 +0000

    aa

--- api/amer.py
+++ api/amer.py
@@ -20,5 +20,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
+        self.wfile.write("hi".encode('utf-8'))
         return
