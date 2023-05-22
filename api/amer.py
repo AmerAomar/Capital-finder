21e0affb6f33acfc71bbbdd82512278e3a464977
AmerAomar <ameralamriii87@gmail.com>
2023-05-22 07:37:08 +0000

    kf

--- api/amer.py
+++ api/amer.py
@@ -1,24 +1,22 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
- 
+
 class handler(BaseHTTPRequestHandler):
- 
+
     def do_GET(self):
-        s = self.path # path is attribute of BaseHTTPRequestHandler
-        url_components = parse.urlsplit(s) 
-        query_string_list=parse.parse_qsl(url_components.query) #should return a list of tuples
-        dictionary=dict(query_string_list) #convert list of tuples to dictionary
-        name=dictionary['name']
-        age=dictionary['age']
+        s = self.path  # path is an attribute of BaseHTTPRequestHandler
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)  # should return a list of tuples
+        dictionary = dict(query_string_list)  # convert the list of tuples to a dictionary
+        name = dictionary.get('name')
+        age = dictionary.get('age')
         if name:
             message = "Hello, " + name + "!"
-
         else:
             message = "Hello, stranger!"
 
-
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write("hi".encode('utf-8'))
         return
